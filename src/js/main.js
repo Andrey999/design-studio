1c7d4079777da4f88a99b7a2a9ea4fe2bbc64bfb
Gromov Andrey <gromovandrey1985@gmail.com>
2019-10-01 01:37:54 +0300

    commit burger menu style and header finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 window.onload = () => {
-  console.log('work');
+  
+  //  menu hamburger
+  let button = document.querySelector('.hamburger');
+  let nav = document.querySelector('.menu .menu__list');
+
+  button.addEventListener('click', function() {
+    this.classList.toggle('hamburger--active');
+    nav.classList.toggle('menu__list--active')
+  });
+
+
 };
